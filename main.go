1d053eda5ea000a781ae748fbad0ca7f4c85067d
Rosco Nap <rosco@MacBook-Pro-van-Rosco.local>
2018-10-23 13:36:31 +0200

    Cleanup doc generate

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/cloudrkt/statuspage-cli/cmd"
-	"github.com/spf13/cobra/doc"
 )
 
 func main() {
 	cmd.Execute()
-
-	err := doc.GenMarkdownTree(cmd.RootCmd, "/tmp")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
